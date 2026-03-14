158c3c329d29bbad701ba45fc4928dab1bd97eb4
omsaimodukwar <omsaimodukwar@gmail.com>
2026-03-14 13:37:42 +0530

    Add files via upload

---
+++ 20260314-133450.c add.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a,b,c;
+ printf("enter the value of a and b\n");
+ scanf("%d%d",&a,&b);
+ c=a+b;
+ printf("addition is:%d",c);
+ getch();
+}
